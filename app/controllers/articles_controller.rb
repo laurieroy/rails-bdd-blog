9b3eec3581648a32331a30ddfeccc8b69245cd42
lauretteroy <laurieroy.dev@gmail.com>
2022-08-17 09:46:29 -0600

    Add sad path creating article, article validations

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,19 +2,26 @@ class ArticlesController < ApplicationController
   def index
   end
 
+  def  show
+	end
+  
   def new
     @article = Article.new
   end
  
   def create
     @article = Article.new(article_params)
-    @article.save
-    flash[:success] = "Article has been created"
-    redirect_to articles_path
+    if @article.save
+      flash[:success] = "Article has been created"
+      redirect_to articles_path
+    else
+      flash[:danger] = "Article has not been created"
+      render :new
+    end
   end
 
   private
-  
+
   def article_params
     params.require(:article).permit(:title, :body)
   end
